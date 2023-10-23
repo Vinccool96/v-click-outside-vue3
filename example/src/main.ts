bc93d2ae46a1aaec2f904df93fcdc127c6a39e3d
Vincent Girard <vinccool96@gmail.com>
2023-10-23 12:39:26 -0400

    fix

--- example/src/main.ts
+++ example/src/main.ts
@@ -1,11 +1,11 @@
 import { createApp } from "vue"
 import "./style.css"
-import { clickOutSide } from "v-click-outside-vue3"
+import { clickOutside } from "v-click-outside-vue3"
 
 import App from "./App.vue"
 
 const app = createApp(App)
 
-app.directive("click-outside", clickOutSide)
+app.directive("click-outside", clickOutside)
 
 app.mount("#app")
